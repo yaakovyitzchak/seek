fe647bc38bcad475cf71c97f066ca3cc474ba3d7
yaakovyitzchak <46662394+yaakovyitzchak@users.noreply.github.com>
2021-06-16 13:46:47 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,18 +4,20 @@ var h = "קראטוןםלפךףתצמנהבסזשדגכעיח_-098756432$1ץADwQ
 r.createServer((q,r) => {
   r.setHeader('content-type', 'text/html');
   var sb = q.url.substring(1)
-  
+  try {
+	sb = decodeURI(sb)	  
+  }catch(e){}
     r.end("<!DOCTYPE><html><head></head><body><style>body{font-family:helvetica</style><h1>B\"H</h1><br>"+
-	 ( sb ? ("url:"+q.url+"<br>string:"+sb+"<br>"+rinvert(sb)) : "Moshiach NOW?!")+"</body></html>")
+	 ( sb ? (rinvert(sb)) : "Moshiach NOW?!")+"</body></html>")
 }).listen(process.env.PORT || 442)
 
 
 function rinvert(str) {
-	return (str.split("").map(x => 
+	return atob(str.split("").map(x => 
 		(i=>(i > -1 && i < n.length ? n[i] : n[0]))(h.indexOf(x))
 	).join(""))
 }
-
+//"url:"+q.url+"<br>string:"+sb+"<br>"+
 
   
   function atob(b64) {
