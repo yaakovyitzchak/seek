9ea0ae241fe8ada502027c497c8528ddbf377251
yaakovyitzchak <46662394+yaakovyitzchak@users.noreply.github.com>
2021-06-16 13:44:25 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ var h = "קראטוןםלפךףתצמנהבסזשדגכעיח_-098756432$1ץADwQ
 r.createServer((q,r) => {
   r.setHeader('content-type', 'text/html');
   var sb = q.url.substring(1)
+  
     r.end("<!DOCTYPE><html><head></head><body><style>body{font-family:helvetica</style><h1>B\"H</h1><br>"+
-	 ( sb ? rinvert(sb) : "Moshiach NOW?!")+"</body></html>")
+	 ( sb ? ("url:"+q.url+"<br>string:"+sb+"<br>"+rinvert(sb)) : "Moshiach NOW?!")+"</body></html>")
 }).listen(process.env.PORT || 442)
 
 
